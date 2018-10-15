7e1aaf6ba194625f498b144b39135ca34f182b41
nnduyquang@gmail.com <bunvodich>
2018-10-15 13:47:54 +0700

    ok

--- local/resources/views/frontend/master.blade.php
+++ local/resources/views/frontend/master.blade.php
@@ -130,6 +130,19 @@
 </script>
 {{ Html::script('js/scripts.js') }}
 @yield('jv-scripts')
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/5bc4198e61d0b77092513d9f/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 <div class="callback d-lg-none d-md-none">
     <div class="phone_animation">
         <div class="phone_animation_circle"></div>
